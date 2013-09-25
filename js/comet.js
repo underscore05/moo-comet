5aeac77b735b00cbee8bedeab00e75a575ddee60
Richard Roque <richardroque@com-mac.local>
2013-09-26 05:18:34 +0800

    Add catcher to request timeout and delay per request

--- js/comet.js
+++ js/comet.js
@@ -16,32 +16,40 @@ var Comet = new Class({
 	Extends: Request.JSON,
 	_try: 0,
 	options: {
+		delayPerRequest: 10,
 		max: 3,
 		data: {}
 	},
 	initialize: function(options){
+		var self = this;
 		this.parent(options);
+		this.addEvents({
+			onFailure: function() { self.send(); },
+			onTimeout: function() { self.send(); },
+			onException: function() { self.send(); }
+		});
 	},
 	send: function(options){
-		this._try++;
-		this.parent(options);
-	},
-	onSuccess: function(json, text){		
-		this.parent(json, text);
-		var changed = (json.timestamp != this.options.data.timestamp);
 		var exceeds = (this._try >= this.options.max);
-		
 		if(exceeds) {
 			this._try = 0;
 			this.fireEvent('maxRequestExceeded');
 			return;
-		}		
-		if(changed) {			
+		}
+		this._try++;
+		this.parent(options);
+	},
+	onSuccess: function(json, text){		
+		var self = this;
+		this.parent(json, text);
+		var changed = (json.timestamp != this.options.data.timestamp);		
+		if(changed) {
 			this.options.data.timestamp = json.timestamp;			
 			this._try = 0;		
 			this.fireEvent('newData', json);
-		}				
-		this.send();	
+		}
+		console.log("Sending another request after "+this.options.delayPerRequest+" seconds...");		
+		this.send.delay(this.options.delayPerRequest * 1000, self);	
 	}
 });
 
